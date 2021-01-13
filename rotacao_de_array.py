743a7dc3ccd3f3232f9075d37b3af24648cfa716
Tânio Scherer <tanioms3@gmail.com>
2021-01-13 14:03:37 -0300

    Segunda implementação da rotação de array

--- rotacao_de_array.py
+++ rotacao_de_array.py
@@ -2,20 +2,37 @@
 Fonte: https://www.programcreek.com/2015/03/rotate-array-in-java/
 """
 
+from itertools import chain
 
-def rotacionar(lista: list, k: int) -> list:
+
+def rotacionar_1(lista: list, k: int) -> list:
     """
->>> rotacionar([1, 2, 3, 4, 5, 6, 7], 3)
-[5, 6, 7, 1, 2, 3, 4]
+    >>> lista = [1, 2, 3, 4, 5, 6, 7]
+    >>> rotacionar_1(lista, 3)
+    [5, 6, 7, 1, 2, 3, 4]
+    >>> rotacionar_1(lista, 5)
+    [3, 4, 5, 6, 7, 1, 2]
     """
-    resultado = []
-    cursor = len(lista)-k
+    tamanho = len(lista)
+    parte_1 = lista[tamanho-k:]
+    parte_2 = lista[:tamanho-k]
+    return parte_1 + parte_2
 
-    for _ in lista:
-        resultado.append(lista[cursor])
-        cursor += 1
 
-        if cursor == len(lista):
-            cursor = 0
+def rotacionar_2(lista: list, k: int):
+    """
+    >>> lista = [1, 2, 3, 4, 5, 6, 7]
+    >>> list(rotacionar_2(lista, 3))
+    [5, 6, 7, 1, 2, 3, 4]
+    >>> list(rotacionar_2(lista, 5))
+    [3, 4, 5, 6, 7, 1, 2]
+    >>> next(rotacionar_2(lista, 3))   # O(1) em tempo e memória
+    5
+    """
+    tamanho = len(lista)
+    parte_1 = range(tamanho-k, tamanho)
+    parte_2 = range(tamanho-k)
+    sequencia = chain(parte_1, parte_2)
 
-    return resultado
+    for i in sequencia:
+        yield lista[i]
